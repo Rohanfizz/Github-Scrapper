e87f4486d16c81f0767e06464da23e75543e2744
Rohanfizz <rohans0501@gmail.com>
2021-11-04 02:24:59 +0530

    Issues are displayed in each repo files

--- repoWork.js
+++ repoWork.js
@@ -2,6 +2,7 @@ let request = require('request');
 let fs = require('fs');
 let cheerio = require('cheerio');
 let path = require('path');
+// work("https://github.com/rails/rails","./Answer/test");
 function work(url,folderPath){
     request(url,cb);
     function cb(err,response,html){
@@ -11,9 +12,31 @@ function work(url,folderPath){
             
             let repoName = cheerioSelector(".mr-2.flex-self-stretch").text().trim();
             // console.log("Making file",path.join(folderPath+(repoName+".json")))
-            fs.writeFileSync(path.join(folderPath,(repoName+".json")),"");
+            let issuePage = url+"/issues";
+
+            request(issuePage,cb);
+            let ans = "";
+            function cb(err,response,html){
+                if(err) console.log(err);
+                else{
+                    let cheerioSelector = cheerio.load(html);
+                    let issues = cheerioSelector('.flex-auto.min-width-0.p-2.pr-3.pr-md-2>a');
+                    let ans = "";
+                    for(let i = 0;i<issues.length;i++){
+                        ans += i + " " + cheerioSelector(issues[i]).text() +"\n";
+                    }
+                    // console.log(ans);
+                    fs.writeFileSync(path.join(folderPath,(repoName+".json")),ans);
+                }
+            }
+            
+
+            // fs.writeFileSync(path.join(folderPath,(repoName+".json")),filler(url+"/issues"));
         }
     }
+}
+function filler(url){
+    
 }
 
 module.exports = {
